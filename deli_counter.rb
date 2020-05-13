5235792a478f31e6e2662e181ad62461750740e5
Luis Reyes <luisreyesxv@yahoo.com>
2020-05-13 08:43:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,9 @@ def line (place)
   if place.length == 0
     puts "The line is currently empty."
   else
+    current_line = "The line is currently: "
     place.collect do |person|
-       place.index(person)+1
+       current_line << "#{place.index(person)+1}. #{person}" 
     end
   end
 
