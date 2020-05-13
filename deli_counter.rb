35f45aa5f39a5abbc157ad2c3674bf725364ca7e
Luis Reyes <luisreyesxv@yahoo.com>
2020-05-13 08:37:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line (place)
   if place.length == 0
     puts "The line is currently empty."
   else
-    place.select do |person|
+    place.collect do |person|
        place.index(person)+1
     end
   end
